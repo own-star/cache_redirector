6708956dcf2d7092a3e9fa577b24616ecf79c144
Taras <taras@own-star>
2021-11-28 11:36:08 +0200

    log full img

--- src/handlers/main.erl
+++ src/handlers/main.erl
@@ -87,7 +87,7 @@ get_links({<<"link">>, Tags, Body}) ->
     log:info("Link: ~p", [proplists:get_value(<<"href">>, Tags, <<>>)]),
     {<<"link">>, handle_links(Tags), Body};
 get_links({<<"img">>, Tags, Body}) ->
-    log:info("Img: ~p", [proplists:get_value(<<"src">>, Tags, <<>>)]),
+    log:info("Img: ~p", [Tags]),
     {<<"img">>, handle_links(Tags), Body};
 get_links({<<"a">>, Tags, Body}) ->
     log:info("A: ~p", [proplists:get_value(<<"href">>, Tags, <<>>)]),
