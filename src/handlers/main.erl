83124e965e0da42ab92f729d3c50154802fd3316
Taras <taras@own-star>
2021-11-28 14:49:01 +0200

    back unos

--- src/handlers/main.erl
+++ src/handlers/main.erl
@@ -51,12 +51,12 @@ search_links(<<"\"https://my.", ?TARGET_LIST, Rest/binary>>, Acc) ->
     log:info("[SearchLink] Key: ~p, My Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "\"http://", ?MY_HOST/binary, "/link/", Key/binary, "\"">>);
 
-search_links(<<"\'https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
+search_links(<<"'https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
-    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
+    FullLink = <<"https://", ?TARGET/binary, Link/binary>>,
     Key = get_key(FullLink),
     log:info("[SearchLink] Key: ~p, Uno Url: ~p", [Key, FullLink]),
-    search_links(NewRest, <<Acc/binary, "\"http://", ?MY_HOST/binary, "/link/", Key/binary, "\"">>);
+    search_links(NewRest, <<Acc/binary, "'http://", ?MY_HOST/binary, "/link/", Key/binary, "'">>);
 
 search_links(<<"srcset=\"https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
