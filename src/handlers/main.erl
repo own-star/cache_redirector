3c3d9873fcf5d8eb55b01659b6fbae7989db600b
Taras <taras@own-star>
2021-11-28 13:21:10 +0200

    remove ''

--- src/handlers/main.erl
+++ src/handlers/main.erl
@@ -51,12 +51,12 @@ search_links(<<"\"https://my.", ?TARGET_LIST, Rest/binary>>, Acc) ->
     log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "\"http://", ?MY_HOST/binary, "/link/", Key/binary, "\"">>);
 
-search_links(<<"\'https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
-    {Link, NewRest} = get_link(Rest),
-    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
-    Key = get_key(FullLink),
-    log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
-    search_links(NewRest, <<Acc/binary, "\'http://", ?MY_HOST/binary, "/link/", Key/binary, "\'">>);
+%search_links(<<"\'https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
+%    {Link, NewRest} = get_link(Rest),
+%    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
+%    Key = get_key(FullLink),
+%    log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
+%    search_links(NewRest, <<Acc/binary, "\'http://", ?MY_HOST/binary, "/link/", Key/binary, "\'">>);
 
 search_links(<<"\"https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
