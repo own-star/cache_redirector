32ca0eb2f105ae53d7d3dc1e1e6534f87631bb0f
Taras <taras@own-star>
2021-11-28 13:08:53 +0200

    remove terminators

--- src/handlers/main.erl
+++ src/handlers/main.erl
@@ -172,25 +172,25 @@ search_links(Page) ->
 
 search_links(<<"\"https://my.", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
-    FullLink = << "\"https://my.", ?TARGET/binary, Link/binary>>,
+    FullLink = << "https://my.", ?TARGET/binary, Link/binary>>,
     Key = get_key(FullLink),
     log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "\"http://", ?MY_HOST/binary, "/link/", Key/binary, "\"">>);
 search_links(<<"\'https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
-    FullLink = << "\'https://", ?TARGET/binary, Link/binary>>,
+    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
     Key = get_key(FullLink),
     log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "\'http://", ?MY_HOST/binary, "/link/", Key/binary, "\'">>);
 search_links(<<"\"https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
-    FullLink = << "\"https://", ?TARGET/binary, Link/binary>>,
+    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
     Key = get_key(FullLink),
     log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "\"http://", ?MY_HOST/binary, "/link/", Key/binary, "\"">>);
 search_links(<<"(https://", ?TARGET_LIST, Rest/binary>>, Acc) ->
     {Link, NewRest} = get_link(Rest),
-    FullLink = << "\"https://", ?TARGET/binary, Link/binary>>,
+    FullLink = << "https://", ?TARGET/binary, Link/binary>>,
     Key = get_key(FullLink),
     log:info("[SearchLink] Key: ~p, Url: ~p", [Key, FullLink]),
     search_links(NewRest, <<Acc/binary, "(http://", ?MY_HOST/binary, "/link/", Key/binary, ")">>);
@@ -209,11 +209,11 @@ get_link(Bin) ->
     get_link(Bin, <<>>).
 
 get_link(<<"\"", Rest/binary>>, Acc) ->
-    {<<Acc/binary, "\"">>, Rest};
+    {<<Acc/binary>>, Rest};
 get_link(<<"\'", Rest/binary>>, Acc) ->
-    {<<Acc/binary, "\'">>, Rest};
+    {<<Acc/binary>>, Rest};
 get_link(<<")", Rest/binary>>, Acc) ->
-    {<<Acc/binary, ")">>, Rest};
+    {<<Acc/binary>>, Rest};
 get_link(<<X, Rest/binary>>, Acc) ->
     get_link(Rest, <<Acc/binary, X>>).
 
