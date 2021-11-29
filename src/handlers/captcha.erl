e4ee898c2d9eaf1d571161b3486c6c01a1447575
Taras <taras@own-star>
2021-11-29 13:14:42 +0200

    fix captcha

--- src/handlers/captcha.erl
+++ src/handlers/captcha.erl
@@ -5,6 +5,6 @@
 -export([init/2]).
 
 init(Req, _) ->
-    {ok, Img, _} = http_service:getURL(<<"https://", ?TARGET/binary, "/captcha.png">>),
+    {ok, Img, _} = http_service:getURL(<<"https://my.", ?TARGET/binary, "/captcha.png">>),
     Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"image/png">>}, Img, Req),
     {ok, Resp, []}.
