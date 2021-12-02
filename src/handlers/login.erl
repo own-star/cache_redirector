7665268697dc9b937c7fd6f3e35354cb67460fc9
Taras <taras@own-star>
2021-12-02 07:26:30 +0200

    add log to login

--- src/handlers/login.erl
+++ src/handlers/login.erl
@@ -7,6 +7,7 @@
 init(Req, _) ->
     Headers = cowboy_req:headers(Req),
     {ok, Data, Req2} = cowboy_req:read_body(Req),
+    log:info("[LOGIN] Headers: ~p", [Headers]),
     case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, Headers) of
         {ok, Json, _} ->
             Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
