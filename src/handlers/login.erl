80555adb73093ce26e9479c8d10e9fee632b8fc4
Taras <taras@own-star>
2021-12-02 07:32:59 +0200

    add new_headers to login

--- src/handlers/login.erl
+++ src/handlers/login.erl
@@ -6,9 +6,12 @@
 
 init(Req, _) ->
     Headers = cowboy_req:headers(Req),
+    NewHeaders = Headers#{<<"host">> => <<"hightech.trade">>,
+                          <<"origin">> => <<"https://my.hightech.trade">>,
+                          <<"referer">> => <<"https://my.hightech.trade/login">>},
     {ok, Data, Req2} = cowboy_req:read_body(Req),
-    log:info("[LOGIN] Headers: ~p", [Headers]),
-    case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, Headers) of
+    log:info("[LOGIN] Headers: ~p", [NewHeaders]),
+    case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, NewHeaders) of
         {ok, Json, _} ->
             Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
             {ok, Resp, []};
