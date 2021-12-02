5c163527f0bbc49bbac9cc931257103151f94b81
Taras <taras@own-star>
2021-12-02 07:23:11 +0200

    add error to login

--- src/handlers/login.erl
+++ src/handlers/login.erl
@@ -7,6 +7,10 @@
 init(Req, _) ->
     Headers = cowboy_req:headers(Req),
     {ok, Data, Req2} = cowboy_req:read_body(Req),
-    {ok, Json, _} = http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, Headers),
-    Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
-    {ok, Resp, []}.
+    case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, Headers) of
+        {ok, Json, _} ->
+            Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
+            {ok, Resp, []};
+        {error, Code, _} ->
+            {ok, cowboy_req:reply(Code, #{}, <<>>, Req2), undefined}
+    end.
