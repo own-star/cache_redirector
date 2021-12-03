b0a899fd8721b5bfef4de1080310169bcd9f1b42
Taras <taras@own-star>
2021-12-03 15:25:25 +0200

    fix headers register

--- src/handlers/register.erl
+++ src/handlers/register.erl
@@ -8,6 +8,6 @@ init(Req, _) ->
 %    Headers = cowboy_req:headers(Req),
     Headers = [{"content-type", "application/x-www-form-urlencoded; charset=UTF-8"}],
     {ok, Data, Req2} = cowboy_req:read_body(Req),
-    {ok, Json, _} = http_service:post(<<"https://my.", ?TARGET/binary, "/auth/register">>, Data, Headers),
-    Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
+    {ok, Json, RespHeaders} = http_service:post(<<"https://my.", ?TARGET/binary, "/auth/register">>, Data, Headers),
+    Resp = cowboy_req:reply(200, maps:from_list(RespHeaders), Json, Req2),
     {ok, Resp, []}.
