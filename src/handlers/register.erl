0f137abb0a8c06960916cc254de9b976ea15167f
Taras <taras@own-star>
2021-12-03 07:49:26 +0200

    fix register

--- src/handlers/register.erl
+++ src/handlers/register.erl
@@ -5,7 +5,8 @@
 -export([init/2]).
 
 init(Req, _) ->
-    Headers = cowboy_req:headers(Req),
+%    Headers = cowboy_req:headers(Req),
+    Headers = [{"content-type", "application/x-www-form-urlencoded; charset=UTF-8"}]
     {ok, Data, Req2} = cowboy_req:read_body(Req),
     {ok, Json, _} = http_service:post(<<"https://my.", ?TARGET/binary, "/auth/register">>, Data, Headers),
     Resp = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json; charset=utf-8">>}, Json, Req2),
