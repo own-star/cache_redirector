c33e9b98a9e4208731c2e31eb2d832f34c195492
Taras <taras@own-star>
2021-12-03 07:40:08 +0200

    fix headers

--- src/handlers/login.erl
+++ src/handlers/login.erl
@@ -6,24 +6,25 @@
 
 init(Req, _) ->
     Headers = cowboy_req:headers(Req),
-    NewHeaders = Headers#{
-                          <<":authority">> => <<"my.hightech.trade">>,
-                          <<":method">> => <<"POST">>,
-                          <<":path">> => <<"/auth/login">>,
-                          <<":scheme">> => <<"https">>,
-                          <<"authority">> => <<"my.hightech.trade">>,
-                          <<"method">> => <<"POST">>,
-                          <<"path">> => <<"/auth/login">>,
-                          <<"scheme">> => <<"https">>,
-                          <<"host">> => <<"hightech.trade">>,
-                          <<"origin">> => <<"https://my.hightech.trade">>,
-                          <<"referer">> => <<"https://my.hightech.trade/login">>,
-                          <<"sec-ch-ua">> => <<"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"">>,
-                          <<"sec-ch-ua-mobile">> => <<"?0">>,
-                          <<"sec-fetch-dest">> => <<"empty">>,
-                          <<"sec-fetch-mode">> => <<"cors">>,
-                          <<"sec-fetch-site">> => <<"same-origin">>
-                  },
+    NewHeaders = [{{"content-type", "application/x-www-form-urlencoded; charset=UTF-8"}}],
+%    NewHeaders = Headers#{
+%                          <<":authority">> => <<"my.hightech.trade">>,
+%                          <<":method">> => <<"POST">>,
+%                          <<":path">> => <<"/auth/login">>,
+%                          <<":scheme">> => <<"https">>,
+%                          <<"authority">> => <<"my.hightech.trade">>,
+%                          <<"method">> => <<"POST">>,
+%                          <<"path">> => <<"/auth/login">>,
+%                          <<"scheme">> => <<"https">>,
+%                          <<"host">> => <<"hightech.trade">>,
+%                          <<"origin">> => <<"https://my.hightech.trade">>,
+%                          <<"referer">> => <<"https://my.hightech.trade/login">>,
+%                          <<"sec-ch-ua">> => <<"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"">>,
+%                          <<"sec-ch-ua-mobile">> => <<"?0">>,
+%                          <<"sec-fetch-dest">> => <<"empty">>,
+%                          <<"sec-fetch-mode">> => <<"cors">>,
+%                          <<"sec-fetch-site">> => <<"same-origin">>
+%                  },
     {ok, Data, Req2} = cowboy_req:read_body(Req),
     log:info("[LOGIN] Headers: ~p", [NewHeaders]),
     case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, maps:to_list(NewHeaders)) of
