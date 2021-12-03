2e406e56ca1fdace0661b9604db67163a485b809
Taras <taras@own-star>
2021-12-03 14:10:26 +0200

    use main

--- src/handlers/login.erl
+++ src/handlers/login.erl
@@ -29,7 +29,10 @@ init(Req, _) ->
 %                  },
     {ok, Data, Req2} = cowboy_req:read_body(Req),
     log:info("[LOGIN] Headers: ~p", [NewHeaders]),
-    case http_service:post(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, NewHeaders) of
+    case main:post_url(<<"https://my.", ?TARGET/binary, "/auth/login">>, Data, NewHeaders) of
+        Body ->
+            Response = cowboy_req:reply(200, #{}, Body, Req2),
+            {ok, Response, []};
         {ok, Json, _} ->
             Resp = cowboy_req:reply(200, #{}, Json, Req2),
             {ok, Resp, []};
